065ac09b24e33fcf2a5e7dda229766dd1c670431
Antonios Papadakis <bdpaboi123@gmail.com>
2023-01-10 11:24:16 -0600

    Update constants

--- hummingbot/connector/exchange/p2p/p2p_constants.py
+++ hummingbot/connector/exchange/p2p/p2p_constants.py
@@ -13,6 +13,9 @@ WSS_URL = "wss://apiws.p2pb2b.{}/"
 PUBLIC_API_VERSION = "v2"
 PRIVATE_API_VERSION = "v2"
 
+# Time endpoint
+CURRENT_TIME_FULL_URL = "https://worldtimeapi.org/api/timezone/America/Chicago"
+
 # Public API endpoints
 TICKER_PRICE_URL = "/public/ticker?market={}"
 EXCHANGE_INFO_PATH_URL = "/public/markets"
@@ -35,8 +38,10 @@ SIDE_SELL = 'sell'
 # P2P has a rate limit that is higher per second, but lower per minute
 # this is the fastest rate limit that will work for both
 # 100 requests per endpoint per minute (Lowest Common Denominator)
-LCD_RATE_LIMIT = 100 / 60
+RATE_PER_ENDPOINT = 10
 ONE_SECOND = 1
+MAX_REQUESTS_PER_SECOND = 9223372036854775807
+
 
 # Order States
 ORDER_STATE = {
@@ -56,11 +61,12 @@ TRADE_EVENT_TYPE = "trade"
 
 RATE_LIMITS = [
     # Pools
-    RateLimit(limit_id=EXCHANGE_INFO_PATH_URL, limit=LCD_RATE_LIMIT, time_interval=ONE_SECOND),
-    RateLimit(limit_id=TICKER_PRICE_URL, limit=LCD_RATE_LIMIT, time_interval=ONE_SECOND),
-    RateLimit(limit_id=BALANCES_PATH_URL, limit=LCD_RATE_LIMIT, time_interval=ONE_SECOND),
-    RateLimit(limit_id=ORDER_HISTORY_PATH_URL, limit=LCD_RATE_LIMIT, time_interval=ONE_SECOND),
-    RateLimit(limit_id=NEW_ORDER_PATH_URL, limit=LCD_RATE_LIMIT, time_interval=ONE_SECOND),
-    RateLimit(limit_id=CANCEL_ORDER_PATH_URL, limit=LCD_RATE_LIMIT, time_interval=ONE_SECOND),
-    RateLimit(limit_id=PENDING_ORDERS_PATH_URL, limit=LCD_RATE_LIMIT, time_interval=ONE_SECOND)
+    RateLimit(limit_id=EXCHANGE_INFO_PATH_URL, limit=RATE_PER_ENDPOINT, time_interval=ONE_SECOND),
+    RateLimit(limit_id=TICKER_PRICE_URL, limit=RATE_PER_ENDPOINT, time_interval=ONE_SECOND),
+    RateLimit(limit_id=BALANCES_PATH_URL, limit=RATE_PER_ENDPOINT, time_interval=ONE_SECOND),
+    RateLimit(limit_id=ORDER_HISTORY_PATH_URL, limit=RATE_PER_ENDPOINT, time_interval=ONE_SECOND),
+    RateLimit(limit_id=NEW_ORDER_PATH_URL, limit=RATE_PER_ENDPOINT, time_interval=ONE_SECOND),
+    RateLimit(limit_id=CANCEL_ORDER_PATH_URL, limit=RATE_PER_ENDPOINT, time_interval=ONE_SECOND),
+    RateLimit(limit_id=PENDING_ORDERS_PATH_URL, limit=RATE_PER_ENDPOINT, time_interval=ONE_SECOND),
+    RateLimit(limit_id=CURRENT_TIME_FULL_URL, limit=MAX_REQUESTS_PER_SECOND, time_interval=ONE_SECOND)
 ]
